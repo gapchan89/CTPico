05fa676e918ddd693b6bebaa167fb6018b1f4fd7
Justin Ou Yang <justinouyang2@gmail.com>
2014-02-03 22:46:02 +0800

    Update Character and CharAI class functions
    Character AI updates the states of the character in the update function.

--- Classes/Character/Character.h
+++ Classes/Character/Character.h
@@ -2,11 +2,12 @@
 #define __CHARACTER_H__
 
 #include "cocos2d.h"
+#include "EnumCharacterState.h"
 
 using namespace cocos2d;
 
 class GameMap;
-//class LevelMode; //TOFIX
+//class LevelMode; //TODO: Fix it Felix
 
 class Character : public CCSprite
 {
@@ -16,47 +17,45 @@ private:
 
 	CCSprite*			_pCharSprite;			// Sprite to store character's sprite sheet
 	CCAnimation*		_pCharAnimation;		// Animation for character
-
-	//CharacterStateEnum	_charState;				// Run/Stop state for Character
 	
-	int					_speedX;
-	int					_speedY;
 	int					_health;
 	int					_immunityDuration;
-	int					_obstacleHitChance;
 
 	void				setupSprite();
 
 public:
+
+	//===== CONSTRUCTOR =====
 	Character(); //Default constructor, temp
 	Character(GameMap* map);
+
+	//===== DESTRUCTOR =====
 	~Character(void);
 	//Character* 				gameSpriteWithFrame(CCSpriteFrame *frame);
-
-	CC_SYNTHESIZE(CCPoint, _nextPosition, NextPosition);
-	CC_SYNTHESIZE(CCPoint, _vector, Vector);
-	CC_SYNTHESIZE(CCTouch *, _touch, Touch);
- 
-	int						getSpeedX()				{	return _speedX;		}
-	void					setSpeedX(int speedX)	{	_speedX = speedX;	}
 	
-	int						getSpeedY()				{	return _speedY;		}
-	void					setSpeedY(int speedY)	{	_speedY = speedY;	}
-	
-	int						getHealth()				{	return _health;		}
-
-	int						getImmunityDuration()	{	return _immunityDuration;		}
-	
-	int						getObstacleHitChance()						{	return _obstacleHitChance;				}
-	void					setObstacleHitChance(int obstacleHitChance)	{	_obstacleHitChance = obstacleHitChance;	}
-	
+	//===== FUNCTIONS =====
 	static Character*		gameSpriteWithFile(const char* filename);
 	static Character* 		gameSpriteWithFrame(CCSpriteFrame *frame);
+	CCRect 					getRect();
 	virtual void 			setPosition(const CCPoint& pos);
 
 	//void 					setupAI(LevelMode levelMode);	// Choose which AI to set up based on level mode
 	bool					reduceHealth(int health);		// Reduce character's health, if < 0, character dies
 	void					update(float timeDiff);			// Update the character's action
+
+	//===== GETTERS/SETTERS =====
+	CC_SYNTHESIZE(CCPoint, _nextPosition, NextPosition);
+	CC_SYNTHESIZE(CCPoint, _vector, Vector);
+	CC_SYNTHESIZE(CCTouch *, _touch, Touch);
+
+	CC_SYNTHESIZE(int, _speedX, SpeedX);
+	CC_SYNTHESIZE(int, _speedY, SpeedY);
+	CC_SYNTHESIZE(int, _obstacleHitChance, ObstacleHitChance);
+
+	CC_SYNTHESIZE(EnumCharacterState, _charState, CharState);
+
+	int						getHealth()				{	return _health;		}
+	int						getImmunityDuration()	{	return _immunityDuration;		}
 
 };
 
--- Classes/Character/CharacterAI.h
+++ Classes/Character/CharacterAI.h
@@ -14,11 +14,19 @@ private:
 
 	Character*			_charReference;
 	GameMap*			_mapReference;
+	float				_amountMoved;		// Updates each time the character 'moves'
+	bool				_isStop;			// Check is Character is told to stop
+
+	bool				isNewGrid();
 
 public:
+	//===== CONSTRUCTOR =====
 	CharacterAI(Character* character, GameMap* map);
+
+	//===== DESTRUCTOR =====
 	~CharacterAI(void);
 
+	//===== FUNCTIONS =====
 	void 				update(float timeDiff);
 };
 
