1b362c2910118df32e5d8e22867da1e78af552a2
FatsGW <guowei_tan@yahoo.com>
2014-02-06 23:34:42 +0800

    Updated CatsManager

--- Classes/Cats/CatsManager.h
+++ Classes/Cats/CatsManager.h
@@ -5,22 +5,37 @@
 #include "Character/Character.h"
 #include "Map/GameMap.h"
 #include "EnumLevelType.h"
+#include "Levels/CatSpawnScript.h"
+#include "Levels/CatSpawnSet.h"
+#include "Cats/BaseCat.h"
+#include "EnumCatState.h"
+#include "GameManager.h"
 
 class CatsManager
 {
 private:
-	GameMap* _pMapRef;
-	Character* _pCharRef;
+	GameMap* 	_pMapRef;
+	Character* 	_pCharRef;
+	GameManager* _pGmRef;
+	int 		_levelType;
+	CCArray* 	_cats;
+	CatSpawnScript* _pSpawnScript;
+	CatSpawnScript* _pDirectorSpawnScript; //for ai director insertion
+
+	void runSpawnScript(float timeDiff);
+	void runDirectorSpawnScript(float timeDiff);
 
 public:
 	//========== CONSTRUCTOR ==========
-	CatsManager(void);
+	CatsManager(int levelType, GameMap* pMapRef, Character* pCharRef, GameManager* pGmRef);
 	//========== DESTRUCTOR ==========
 	~CatsManager(void);
 
-	//========== FUNCTIONS ==========s
+	//========== FUNCTIONS ==========
 	void update(float timeDiff);
-	void setupAI(EnumLevelType levelType);
+	void setLevelSpawnScript(CatSpawnScript* pCatSpawnScript);
+	void insertDirectorSpawnSet(CatSpawnSet* pCatSpawnSet);
+
 };
 
 #endif // __CATSMANAGER__
