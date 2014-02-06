e76b6b0c533832acc6ae42dd6c0a9d42614d0500
FatsGW <guowei_tan@yahoo.com>
2014-02-06 23:34:42 +0800

    Updated CatsManager

--- Classes/Cats/CatsManager.cpp
+++ Classes/Cats/CatsManager.cpp
@@ -7,14 +7,75 @@ using namespace CocosDenshion;
 
 //========== CONSTRUCTOR ==========
 
-CatsManager::CatsManager()
+CatsManager::CatsManager(int levelType, GameMap* pMapRef, Character* pCharRef, GameManager* pGmRef)
 {
+	_levelType = levelType;
+	_pMapRef = pMapRef;
+	_pCharRef = pCharRef;
+	_pGmRef = pGmRef;
+
+	_pSpawnScript = 0;
+	_pDirectorSpawnScript = 0;
+
+	_cats = new CCArray();
 }
 
 //========== DESTRUCTOR ==========
 
 CatsManager::~CatsManager()
 {
+	_pMapRef = 0;
+	_pCharRef = 0;
+
+	CCObject* cat = 0;
+	CCARRAY_FOREACH(_cats, cat)
+	{
+		BaseCat* pCat = dynamic_cast<BaseCat*>(cat);
+		delete pCat;
+	}
+
+	_cats->release();
+	delete _cats;
 }
 
 //========== FUNCTIONS ==========
+void CatsManager::update(float timeDiff)
+{
+	//check spawn script
+	runSpawnScript(timeDiff);
+	//check additional spawn script
+	runDirectorSpawnScript(timeDiff);
+
+	//update all cats
+	CCObject* cat = 0;
+	CCARRAY_FOREACH(_cats, cat)
+	{
+		BaseCat* pCat = dynamic_cast<BaseCat*>(cat);
+		pCat->update(timeDiff, _pMapRef, _pCharRef);
+	}
+
+	//remove any cats that is marked for deletion
+	CCARRAY_FOREACH(_cats, cat)
+	{
+		BaseCat* pCat = dynamic_cast<BaseCat*>(cat);
+		if (pCat->getState() == CAT_DEAD)
+		{
+			_pGmRef->removeChild(pCat);
+			_cats->removeObject(pCat, true);
+			delete pCat;
+		}
+	}
+}
+
+void CatsManager::setLevelSpawnScript(CatSpawnScript* pCatSpawnScript)
+{
+	_pSpawnScript = pCatSpawnScript;
+}
+
+void CatsManager::insertDirectorSpawnSet(CatSpawnSet* pCatSpawnSet)
+{
+
+}
+
+
+
